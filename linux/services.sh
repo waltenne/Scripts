556393a8187ef114addfdbe65b30c4f122db71f4
waltenne <38992117+waltenne@users.noreply.github.com>
2020-03-06 11:52:18 -0300

    Update services.sh

--- linux/services.sh
+++ linux/services.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NAMESERVICE[0]=activemq
-NAMESERVICE[1]=tomcat
+NAMESERVICE[0]=apache2
+NAMESERVICE[1]=docker
 
-NSERVICEEXIST[0]=`ls /etc/init.d/ | grep -m1 'activemq' | wc -l`
-NSERVICEEXIST[1]=`ls /etc/init.d/ | grep -m1 'tomcat' | wc -l`
+NSERVICEEXIST[0]=$(find /etc/init.d/ -name apache2 | wc -l)
+NSERVICEEXIST[1]=$(find /etc/init.d/ -name docker | wc -l)
 
 running0="active(running)"
 running1="is running"
@@ -23,18 +23,18 @@ echo -ne "\n1. Check Status"
 echo -ne "\n2. Start Services"
 echo -ne "\n3. Stop  Services"
 echo -ne "\n4. Return Menu"
-echo -ne "\nSelect Option: " ; read opcao
-	case $opcao in
+echo -ne "\nSelect Option: " ; read -r option
+	case $option in
 		1) Status ;;
 		2) Starting ;;
 		3) Stopping ;;
 		4) MenuServices ;;
-		*) 2> /dev/null; echo -e "${WHITE}Option Unkown.${NC}" ; Status ;;
+		*) clear && echo -ne "Option Unkown." && sleep 1 ; MenuServices ;;
 		esac
 }
 
 ServiceIsActive(){
-	a=`service $1 status | grep 'Active:' | awk '{print $2 $3}'`
+	a=$(service "$1" status | grep 'Active:' | awk '{print $2 $3}')
 	if [[ "$a" == "$activating" ]]; then
 			echo "0"
 	elif [[ "$a" == "$running0" ]] || [[ "$a" == "$running1" ]] || [[ "$a" == "$running2" ]]; then
@@ -49,27 +49,29 @@ ServiceIsActive(){
 }
 
 Status(){
+for (( a = 0, b = 0; a < ${#NAMESERVICE[*]}, b < ${#NSERVICEEXIST[*]} ; a++, b++ )) ; do
+	if [[ ${NSERVICEEXIST[b]} == "1" ]];
+	then
+		if [[ $(ServiceIsActive ${NAMESERVICE[a]}) == "0" ]]; then
+			echo "activating - ${NAMESERVICE[a]}"
+		elif [[ $(ServiceIsActive ${NAMESERVICE[a]}) == "1" ]]; then
+			echo "running - ${NAMESERVICE[a]}"
+		elif [[ $(ServiceIsActive ${NAMESERVICE[a]}) == "2" ]]; then
+			echo "stopping - ${NAMESERVICE[a]}"
+		elif [[ $(ServiceIsActive ${NAMESERVICE[a]}) == "3" ]]; then
+			echo "stopped - ${NAMESERVICE[a]}"
+		elif [[ $(ServiceIsActive ${NAMESERVICE[a]}) == "4" ]]; then
+			echo "falied - ${NAMESERVICE[a]}"
+		fi
+	fi
+done
+sleep 3
+SECONDS=5
+for ((i=$SECONDS; i>=1; i--));	do
 	clear
-	for (( x ; x < ${#NAMESERVICE[*]}; x++ )) ; do
-		if [[ $(ServiceIsActive ${NAMESERVICE[x]}) == "0" ]]; then
-				echo "activating - ${NAMESERVICE[x]}"
-		elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) == "1" ]]; then
-				echo "running - ${NAMESERVICE[x]}"
-		elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) == "2" ]]; then
-				echo "stopping - ${NAMESERVICE[x]}"
-		elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) == "3" ]]; then
-				echo "stopped - ${NAMESERVICE[x]}"
-		elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) == "4" ]]; then
-				echo "falied - ${NAMESERVICE[x]}"
-		fi		
-	done
-	sleep 3
-	SECONDS=5
-	for ((i=SECONDS; i>=1; i--));	do
-		clear
-		echo -ne "Return in Menu Services in $i seconds" && sleep 1.5
-	done
-	MenuServices
+	echo -ne "\nReturn in Menu Services in $i seconds" && sleep 1.5
+done
+MenuServices
 }
 
 Stopping(){
@@ -77,26 +79,28 @@ clear
 echo -ne "How do you do?"
 echo -ne "\n1. Stop Services"
 echo -ne "\n2. Return at Services Menu"
-echo -ne "\nSelect Option: " ; read option
+echo -ne "\nSelect Option: " ; read -r option
 	case $option in
 		1)
-			for (( x ; x < ${#NAMESERVICE[*]}; x++ )) ; do 
-					if [[ $(ServiceIsActive ${NAMESERVICE[x]}) == '1' ]];	then
+			for (( x ; x < ${#NAMESERVICE[*]}; x++ )) ; do
+				stoppings=$(ServiceIsActive ${NAMESERVICE[x]})
+					if [[ $stoppings == '1' ]];	then
 						echo "${NAMESERVICE[x]} stopping" && service ${NAMESERVICE[x]} stop
 						while :
 						do
-								if [[ $(ServiceIsActive ${NAMESERVICE[x]}) == '3' ]]; then
+								if [[ $stoppings == '3' ]]; then
 									echo "${NAMESERVICE[x]} stopped"
+									sleep 1.5
 									break
 								fi
 						done
-					elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) != '1' ]];	then
+					elif [[ $stoppings != '1' ]];	then
 						echo "${NAMESERVICE[x]} maybe is stopped, check next service"
 						sleep 1.5
 					fi
 			done
 			SECONDS=5
-			for ((i=SECONDS; i>=1; i--));	do
+			for ((i=$SECONDS; i>=1; i--));	do
 				clear
 				echo -ne "Process completed. Now return in Menu Services in $i seconds" && sleep 1.5
 			done
@@ -105,13 +109,13 @@ echo -ne "\nSelect Option: " ; read option
 		2) 
 			sleep 1
 			SECONDS=5
-			for ((i=SECONDS; i>=1; i--));	do
+			for ((i=$SECONDS; i>=1; i--));	do
 				clear
 				echo -ne "Now Return in Menu Services in $i seconds" && sleep 1.5
 			done
 			MenuServices
 			;;
-		*) 2> /dev/null; echo -e "${WHITE}Option Unkown.${NC}" ; Status ;;
+		*) clear && echo -ne "Option Unkown." && sleep 1 ; Stopping ;;
 		esac
 }
 
@@ -120,26 +124,30 @@ clear
 echo -ne "How do you do?"
 echo -ne "\n1. Start Services"
 echo -ne "\n2. Return at Services Menu"
-echo -ne "\nSelect Option: " ; read option
+echo -ne "\nSelect Option: " ; read -r option
 	case $option in
 		1)
-			for (( x ; x < ${#NAMESERVICE[*]}; x++ )) ; do 
-					if [[ $(ServiceIsActive ${NAMESERVICE[x]}) == '3' ]];	then
-						echo "${NAMESERVICE[x]} starting" && service ${NAMESERVICE[x]} start
+			for (( z ; z < ${#NAMESERVICE[*]}; z++ )) ; do 
+				startings=$(ServiceIsActive ${NAMESERVICE[x]})
+					if [[ $startings == '3' ]];	then
+						echo "${NAMESERVICE[z]} starting" && service ${NAMESERVICE[z]} start
 						while :
 						do
-								if [[ $(ServiceIsActive ${NAMESERVICE[x]}) == '1' ]]; then
-									echo "${NAMESERVICE[x]} started"
+								if [[ $startings == '1' ]]; then
+									echo "${NAMESERVICE[z]} started"
+									sleep 1.5
 									break
 								fi
 						done
-					elif [[ $(ServiceIsActive ${NAMESERVICE[x]}) == '1' ]];	then
-						echo "${NAMESERVICE[x]} is running, check next service"
+					elif [[ $startings == '1' ]];	then
+						echo "${NAMESERVICE[z]} is running, check next service"
 						sleep 1.5
+
 					fi
 			done
+			sleep 1.5
 			SECONDS=5
-			for ((i=SECONDS; i>=1; i--));	do
+			for ((i=$SECONDS; i>=1; i--));	do
 				clear
 				echo -ne "Process completed. Return in Services Menu in $i seconds" && sleep 1.5
 			done
@@ -148,13 +156,13 @@ echo -ne "\nSelect Option: " ; read option
 		2) 
 			sleep 1
 			SECONDS=5
-			for ((i=SECONDS; i>=1; i--));	do
+			for ((i=$SECONDS; i>=1; i--));	do
 				clear
 				echo -ne "Return in Services Menu in $i seconds" && sleep 1.5
 			done
 			MenuServices
 			;;
-		*) 2> /dev/null; echo -e "${WHITE}Option Unkown.${NC}" ; Status ;;
+		*) clear && echo -ne "Option Unkown." && sleep 1 ; Starting ;;
 		esac
 }
 
